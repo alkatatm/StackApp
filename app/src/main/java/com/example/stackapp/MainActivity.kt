4597e0cc336037c2ee2a49743cac462ac8f0c425
Mustafa Al-Katat <alkatat.m@gmail.com>
2025-11-06 20:12:49 -0800

    Pop/Push functions finished. Debugging of stack done

--- app/src/main/java/com/example/stackapp/MainActivity.kt
+++ app/src/main/java/com/example/stackapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.stackapp
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import  android.util.Log
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,20 +15,45 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.stackapp.ui.theme.StackAppTheme
 
 val myStack = IntArray(3)
-
+val newStack = IntArray(3)
+var stackIndex = 0
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d("Initial", myStack.contentToString())
+        push(myStack, 3)
+        Log.d("Push1Stack", myStack.contentToString())
+        push(myStack, 7)
+        Log.d("Push2Stack", myStack.contentToString())
+        pop(myStack)
+        Log.d("Pop1Stack", newStack.contentToString())
+        push(myStack, 5)
+        Log.d("Push3Stack", myStack.contentToString())
+        push(myStack, 2)
+        Log.d("Push4Stack", myStack.contentToString())
+        pop(myStack)
+        Log.d("Pop2Stack", newStack.contentToString())
+
     }
 }
 
 
-fun Pop (myStack: IntArray)
+fun pop (myStack: IntArray)
 {
-
+    myStack.copyInto(newStack,0,0, stackIndex - 1)
+    newStack.copyInto(myStack)
+    if(stackIndex >= 0)
+        stackIndex--
+    else
+        stackIndex = 2
 }
 
-fun Push (myStack: IntArray)
+fun push (myStack: IntArray, num: Int)
 {
+    myStack[stackIndex] = num
+    if(stackIndex <= 2)
+        stackIndex++
+    else
+        stackIndex = 0
 
 }
